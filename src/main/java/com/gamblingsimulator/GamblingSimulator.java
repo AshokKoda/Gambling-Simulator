332da61692f679bf62637de52fb0d000746879f9
Ashok Koda <kodaashok60@gmail.com>
2022-03-21 20:22:42 +0530

    Added:Initializing variables

--- src/main/java/com/gamblingsimulator/GamblingSimulator.java
+++ src/main/java/com/gamblingsimulator/GamblingSimulator.java
@@ -1,9 +1,15 @@
 package com.gamblingsimulator;
 
 public class GamblingSimulator {
+	
+	static int dailyStake = 100;
+    static int betInDollar = 1;
 
 	public static void main(String[] args) {
 		System.out.println("************Welcome To Gambling Simulator***************");
+		
+		System.out.println("Total daily Stake is : " + dailyStake);
+        System.out.println("Betting Dollar for every game : " + betInDollar);
 
 	}
 
