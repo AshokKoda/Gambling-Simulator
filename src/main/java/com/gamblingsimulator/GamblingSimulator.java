fa51d05c1e550ff5978735783653d8c9adefaba5
Ashok Koda <kodaashok60@gmail.com>
2022-03-21 21:29:21 +0530

    Added:Resign for day upto 50% reached

--- src/main/java/com/gamblingsimulator/GamblingSimulator.java
+++ src/main/java/com/gamblingsimulator/GamblingSimulator.java
@@ -5,20 +5,21 @@ public class GamblingSimulator {
 	public static int dailyStake = 100;
 	public static int betInDollar = 1;
 	public static final int win = 1;
-//	public static final int loss = 0;
 
 	public static void main(String[] args) {
 		System.out.println("************Welcome To Gambling Simulator***************");
-
-		int result = (int) ((Math.random() * 10) % 2);
-		if(result == win) {
-			dailyStake++;
-			System.out.println("Won");
-		}else {
-			dailyStake--;
-			System.out.println("Loose");
-		}
 		
+		while (dailyStake > 50 && dailyStake != 150 ) {
+			int result = (int) ((Math.random() * 10) % 2);
+			if(result == win) {
+				dailyStake++;
+				System.out.println("Won");
+			}else {
+				dailyStake--;
+				System.out.println("Loose");
+			}
+		}
+
 		System.out.println("Total daily Stake is : " + dailyStake);
         System.out.println("Betting Dollar for every game : " + betInDollar);
 	}
