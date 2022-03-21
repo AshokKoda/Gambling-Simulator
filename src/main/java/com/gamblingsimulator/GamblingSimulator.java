81d341641fde90716ae91434544ae6ee5bc7822a
Ashok Koda <kodaashok60@gmail.com>
2022-03-21 20:04:22 +0530

    Added:Welcome to Gambling simulator

---
+++ src/main/java/com/gamblingsimulator/GamblingSimulator.java
@@ -0,0 +1,10 @@
+package com.gamblingsimulator;
+
+public class GamblingSimulator {
+
+	public static void main(String[] args) {
+		System.out.println("************Welcome To Gambling Simulator***************");
+
+	}
+
+}
