5058fff104f4273d808f4b1f10e3d13cc6186988
Ashok Koda <kodaashok60@gmail.com>
2022-03-21 20:37:50 +0530

    Added:If win make amount or loose decrease amount

--- src/main/java/com/gamblingsimulator/GamblingSimulator.java
+++ src/main/java/com/gamblingsimulator/GamblingSimulator.java
@@ -1,16 +1,26 @@
 package com.gamblingsimulator;
 
 public class GamblingSimulator {
-	
-	static int dailyStake = 100;
-    static int betInDollar = 1;
+
+	public static int dailyStake = 100;
+	public static int betInDollar = 1;
+	public static final int win = 1;
+//	public static final int loss = 0;
 
 	public static void main(String[] args) {
 		System.out.println("************Welcome To Gambling Simulator***************");
+
+		int result = (int) ((Math.random() * 10) % 2);
+		if(result == win) {
+			dailyStake++;
+			System.out.println("Won");
+		}else {
+			dailyStake--;
+			System.out.println("Loose");
+		}
 		
 		System.out.println("Total daily Stake is : " + dailyStake);
         System.out.println("Betting Dollar for every game : " + betInDollar);
-
 	}
 
 }
